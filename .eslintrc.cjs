0dbea9ba3b4e1fc6ef30ff2bb5ee5394d5c39f76
Nam Nguyen <hoangnamnguyen191@gmail.com>
2023-07-25 21:44:56 -0400

    feat: working state v1

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,21 +7,21 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:react-hooks/recommended',
+    'plugin:react-hooks/recommended'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
     project: true,
-    tsconfigRootDir: __dirname,
+    tsconfigRootDir: __dirname
   },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
-      { allowConstantExport: true },
+      { allowConstantExport: true }
     ],
-    '@typescript-eslint/no-non-null-assertion': 'off',
-  },
-}
+    '@typescript-eslint/no-non-null-assertion': 'off'
+  }
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,121 @@
+import MenuIcon from '@mui/icons-material/Menu';
+import {
+  AppBar,
+  Box,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Toolbar
+} from '@mui/material';
+import Grid from '@mui/material/Unstable_Grid2';
 import React, { useState } from 'react';
-import styles from './App.module.scss';
+
+type Video = { name: string; url: string; isPlaying: boolean };
 
 function App() {
   const [videoSrc, setVideoSrc] = useState<string | undefined>();
+  const [openDrawer, setOpenDrawer] = useState<boolean>(false);
+  const [videosList, setVideosList] = useState<Video[]>([]);
 
-  const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const play = (): void => {
+    videosList.forEach((vid) => {
+      if (vid.isPlaying) {
+        setVideoSrc(vid.url);
+      }
+    });
+  };
+
+  const setNextTrack = (jumpToIndex?: number): void => {
+    const currentVidIndex = videosList.findIndex((vid) => vid.isPlaying);
+    videosList[currentVidIndex].isPlaying = false;
+
+    if (jumpToIndex !== undefined) {
+      videosList[jumpToIndex].isPlaying = true;
+    } else {
+      videosList[
+        currentVidIndex === videosList.length - 1 ? 0 : currentVidIndex + 1
+      ].isPlaying = true;
+    }
+  };
+
+  const onInputChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
     const files: FileList = e.target.files!;
 
-    for (const [key, value] of Object.entries(files)) {
-      console.log(`${key}: ${value.type}`);
+    const videoList: Video[] = [];
+    for (const [index, value] of Object.entries(files)) {
+      const fileName = value.name;
+      const fileNameWithoutExt =
+        fileName.substring(0, fileName.lastIndexOf('.')) || fileName;
+      const fileURL = URL.createObjectURL(value);
+      if (parseInt(index) === 0) {
+        setVideoSrc(fileURL);
+      }
+      videoList.push({
+        name: fileNameWithoutExt,
+        url: fileURL,
+        isPlaying: parseInt(index) === 0 ? true : false
+      });
     }
 
-    const fileURL = URL.createObjectURL(files[0]);
+    setVideosList(videoList);
+  };
 
-    setVideoSrc(() => fileURL);
+  const onVideoEnded = (): void => {
+    setNextTrack();
+    play();
   };
 
   return (
-    <div className={styles.PlayerContainer}>
-      <div className={styles.InputContainer}>
+    <Box sx={{ flexGrow: 1 }}>
+      <Drawer open={openDrawer} onClose={() => setOpenDrawer(false)}>
+        <h1>Hello</h1>
         <input type="file" accept="video/*" multiple onChange={onInputChange} />
-      </div>
-      <div className={styles.VideosList}>
-        <h2>Play List</h2>
-      </div>
-      <div className={styles.VideoFrame}>
-        <video controls autoPlay src={videoSrc}></video>
-      </div>
-    </div>
+        <List>
+          {videosList.map((video, index) => (
+            <ListItem disablePadding key={video.url}>
+              <ListItemButton>
+                <ListItemText
+                  primary={video.name}
+                  onClick={() => {
+                    setNextTrack(index);
+                    play();
+                  }}
+                />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+            onClick={() => setOpenDrawer(true)}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+
+      <Grid container spacing={2}>
+        <Grid xs={12} alignItems={'center'}>
+          <video
+            style={{ width: '100%', height: '70vh' }}
+            controls
+            autoPlay
+            src={videoSrc}
+            onEnded={onVideoEnded}
+          ></video>
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
 

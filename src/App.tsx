b31c950f5172cb50aa25371cf084549a1548ea34
Nam Nguyen <hoangnamnguyen191@gmail.com>
2023-07-26 23:18:59 -0400

    feat: style list and refractor

--- src/App.tsx
+++ src/App.tsx
@@ -1,93 +1,94 @@
 import MenuIcon from '@mui/icons-material/Menu';
+import PlayArrow from '@mui/icons-material/PlayArrow';
 import {
   AppBar,
   Box,
+  Button,
+  Container,
   Drawer,
   IconButton,
   List,
   ListItem,
-  ListItemButton,
-  ListItemText,
-  Toolbar
+  Toolbar,
+  Typography
 } from '@mui/material';
 import Grid from '@mui/material/Unstable_Grid2';
 import React, { useState } from 'react';
 
-type Video = { name: string; url: string; isPlaying: boolean };
+type Video = { name: string; url: string };
 
 function App() {
   const [videoSrc, setVideoSrc] = useState<string | undefined>();
   const [openDrawer, setOpenDrawer] = useState<boolean>(false);
   const [videosList, setVideosList] = useState<Video[]>([]);
+  const [currentPlayedVideoIndex, setCurrentPlayedVideoIndex] =
+    useState<number>(-1);
 
-  const play = (): void => {
-    videosList.forEach((vid) => {
-      if (vid.isPlaying) {
-        setVideoSrc(vid.url);
-      }
-    });
-  };
-
-  const setNextTrack = (jumpToIndex?: number): void => {
-    const currentVidIndex = videosList.findIndex((vid) => vid.isPlaying);
-    videosList[currentVidIndex].isPlaying = false;
-
-    if (jumpToIndex !== undefined) {
-      videosList[jumpToIndex].isPlaying = true;
-    } else {
-      videosList[
-        currentVidIndex === videosList.length - 1 ? 0 : currentVidIndex + 1
-      ].isPlaying = true;
-    }
+  const play = (index: number): void => {
+    setVideoSrc(videosList[index].url);
+    setCurrentPlayedVideoIndex(index);
   };
 
   const onInputChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    const files: FileList = e.target.files!;
+    const files: File[] = [...e.target.files!];
 
     const videoList: Video[] = [];
-    for (const [index, value] of Object.entries(files)) {
-      const fileName = value.name;
+    for (const file of files) {
+      const fileName = file.name;
       const fileNameWithoutExt =
         fileName.substring(0, fileName.lastIndexOf('.')) || fileName;
-      const fileURL = URL.createObjectURL(value);
-      if (parseInt(index) === 0) {
-        setVideoSrc(fileURL);
-      }
+      const fileURL = URL.createObjectURL(file);
+
       videoList.push({
         name: fileNameWithoutExt,
-        url: fileURL,
-        isPlaying: parseInt(index) === 0 ? true : false
+        url: fileURL
       });
     }
 
     setVideosList(videoList);
+    setVideoSrc(videoList[0].url);
+    setCurrentPlayedVideoIndex(0);
   };
 
   const onVideoEnded = (): void => {
-    setNextTrack();
-    play();
+    console.log(`Nam data is: video ended`);
+    const nextVideoIndex =
+      currentPlayedVideoIndex === videosList.length - 1
+        ? 0
+        : currentPlayedVideoIndex + 1;
+    console.log(`Nam data is: nextVideoIndex`, nextVideoIndex);
+    setCurrentPlayedVideoIndex(nextVideoIndex);
+    setVideoSrc(videosList[nextVideoIndex].url);
   };
 
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Drawer open={openDrawer} onClose={() => setOpenDrawer(false)}>
-        <h1>Hello</h1>
-        <input type="file" accept="video/*" multiple onChange={onInputChange} />
-        <List>
-          {videosList.map((video, index) => (
-            <ListItem disablePadding key={video.url}>
-              <ListItemButton>
-                <ListItemText
-                  primary={video.name}
+        <Container>
+          <Typography variant="h2" gutterBottom>
+            Play list
+          </Typography>
+          <input
+            type="file"
+            accept="video/*"
+            multiple
+            onChange={onInputChange}
+          />
+          <List>
+            {videosList.map((video, index) => (
+              <ListItem key={video.url}>
+                <Button
                   onClick={() => {
-                    setNextTrack(index);
-                    play();
+                    play(index);
                   }}
-                />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
+                  endIcon={<PlayArrow />}
+                >
+                  {video.name}
+                </Button>
+              </ListItem>
+            ))}
+          </List>
+        </Container>
       </Drawer>
       <AppBar position="static">
         <Toolbar>
@@ -104,15 +105,24 @@ function App() {
         </Toolbar>
       </AppBar>
 
-      <Grid container spacing={2}>
+      <Grid container spacing={2} padding={4}>
         <Grid xs={12} alignItems={'center'}>
-          <video
-            style={{ width: '100%', height: '70vh' }}
-            controls
-            autoPlay
-            src={videoSrc}
-            onEnded={onVideoEnded}
-          ></video>
+          {videosList.length ? (
+            <>
+              <video
+                style={{ width: '100%', height: '70vh' }}
+                controls
+                autoPlay
+                src={videoSrc}
+                onEnded={() => onVideoEnded()}
+              ></video>
+              <Typography variant="subtitle1">
+                Currently playing: {videosList[currentPlayedVideoIndex].name}
+              </Typography>
+            </>
+          ) : (
+            <Typography variant="body1">Select videos to start</Typography>
+          )}
         </Grid>
       </Grid>
     </Box>
